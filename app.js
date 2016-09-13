25e9fa093981b82ff0a6e6ae82406e0f9e2ef147
Casey Bennington <cbenning@vt.edu>
2016-09-13 00:18:44 -0700

    Partial work on 5 and scoring for 6

--- app.js
+++ app.js
@@ -1,35 +1,8 @@
-function Game() {
-  var canvas = document.getElementById('myCanvas');
-  this.ctx = canvas.getContext('2d');
-
-  this.player = new Player(285, 10);
-  this.computer = new Computer(10, 100);
-  this.ball = new Ball(this.ctx.canvas.width/2, this.ctx.canvas.height/2);
+function Display(x, y) {
+  this.x = x;
+  this.y = y;
+  this.value = 0;
 }
-
-Game.prototype = {
-  render: function() {
-    this.setupCanvas();
-    this.player.render(this.ctx);
-    this.computer.render(this.ctx);
-    this.ball.render(this.ctx);
-  },
-  update: function() {
-    if (this.paused)
-      return;
-  },
-  setupCanvas: function() {
-    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-    this.ctx.setLineDash([5, 2]);
-
-    this.ctx.beginPath();
-    this.ctx.moveTo(this.ctx.canvas.width/2, 0);
-    this.ctx.lineTo(this.ctx.canvas.width/2, this.ctx.canvas.height);
-    this.ctx.strokeStyle="white";
-    this.ctx.stroke();
-    this.ctx.closePath();
-  }
-};
 
 function Paddle(x, y) {
   this.x = x;
@@ -39,6 +12,34 @@ function Paddle(x, y) {
   this.speed = 10;
   this.score = 0;
 }
+
+Paddle.prototype = {
+  render: function(ctx) {
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = "white";
+    ctx.fill();
+  }
+  // },
+  // move: function() {
+  //   if (upPressed && this.y > 0) {
+  //     this.y -= this.speed;
+  //   } else if (downPressed && this.y < game.ctx.canvas.height-this.height) {
+  //     this.y += this.speed;
+  //   }
+  // }
+};
+
+// Player.prototype = new Paddle();
+// Player.prototype.constructor = Player;
+// Computer.prototype = new Paddle();
+// Computer.prototype.constructor = Computer;
+//
+Player.prototype = Object.create(Paddle.prototype);
+Player.prototype.constructor = Player;
+Computer.prototype = Object.create(Paddle.prototype);
+Computer.prototype.constructor = Computer;
+
 function Player(x, y) {
   Paddle.call(this);
   this.x = x;
@@ -58,13 +59,14 @@ function Ball(x, y) {
   this.vy = 2 - Math.abs(this.vx);
 }
 
-Paddle.prototype = {
-  render: function(ctx) {
-    ctx.beginPath();
-    ctx.rect(this.x, this.y, this.width, this.height);
-    ctx.fillStyle = "white";
-    ctx.fill();
-  },
+Computer.prototype = {
+  update: function(ball) {
+    //this.move;
+    console.log(ball);
+  }
+};
+
+Player.prototype = {
   move: function() {
     if (upPressed && this.y > 0) {
       this.y -= this.speed;
@@ -74,10 +76,11 @@ Paddle.prototype = {
   }
 };
 
-Player.prototype = Object.create(Paddle.prototype);
-Player.prototype.constructor = Player;
-Computer.prototype = Object.create(Paddle.prototype);
-Computer.prototype.constructor = Computer;
+Display.prototype = {
+  render: function(ctx) {
+    ctx.fillText(this.value, this.x, this.y);
+  }
+};
 
 Ball.prototype = {
   render: function(ctx) {
@@ -128,6 +131,67 @@ Ball.prototype = {
     if ((this.vy < 0 && this.y < 0) || (this.vy > 0 && this.y + this.height > game.ctx.canvas.height)) {
       this.vy = -this.vy;
     }
+
+    // Determine scoring play
+    if (this.x >= game.width) {
+      game.score(game.computer);
+    } else if (this.x + this.width <= 0) {
+      game.score(game.player);
+    }
+  }
+};
+
+function Game() {
+  var canvas = document.getElementById('myCanvas');
+  this.ctx = canvas.getContext('2d');
+
+  this.player = new Player(285, 10);
+  this.computer = new Computer(10, 100);
+  this.ball = new Ball(this.ctx.canvas.width/2, this.ctx.canvas.height/2);
+  this.display1 = new Display(this.width/4, 25);
+  this.display2 = new Display(this.width*3/4, 25);
+}
+
+Game.prototype = {
+  render: function() {
+    this.setupCanvas();
+    this.player.render(this.ctx);
+    this.computer.render(this.ctx);
+    this.ball.render(this.ctx);
+  },
+  update: function() {
+    if (this.paused)
+      return;
+
+      this.ball.update();
+      this.display1.value = this.computer.score;
+      this.display2.value = this.player.score;
+  },
+  setupCanvas: function() {
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    this.ctx.setLineDash([5, 2]);
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.ctx.canvas.width/2, 0);
+    this.ctx.lineTo(this.ctx.canvas.width/2, this.ctx.canvas.height);
+    this.ctx.strokeStyle="white";
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    this.display1.render(this.ctx);
+    this.display2.render(this.ctx);
+  },
+  score: function(paddle) {
+    paddle.score++;
+    var player = paddle == this.computer ? 0 : 1;
+
+    this.ball.x = this.width/2;
+    this.ball.y = paddle.y + paddle.height/2;
+
+    this.vx = Math.floor(Math.random() * 4 - 4);
+    this.vy = 2 - Math.abs(this.vx);
+    if (player == 1)
+      this.ball.vx *= -1;
   }
 };
 
@@ -168,11 +232,11 @@ var animate =
 function step() {
   game.render();
   game.ball.update();
+  game.computer.update(game.ball);
   animate(step);
 }
 
 function MainFunction() {
-  game.setupCanvas();
   animate(step);
 }
 
